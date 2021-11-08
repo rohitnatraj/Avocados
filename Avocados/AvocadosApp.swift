abdfb89b4d486f0656084867eb3bd99a287b4e1e
Rohit Natraj <rohitnatraj@gmail.com>
2021-11-08 11:50:17 -0700

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Natraj, Rohit on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
